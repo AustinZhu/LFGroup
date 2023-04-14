d131cf1b4166c7d73f0ac0282763732fade824a1
Lmmmmm <lm839377386@gmail.com>
2023-04-15 02:52:27 +0900

    feat: add homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,18 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+'use client';
 
-const inter = Inter({ subsets: ['latin'] })
+import Image from 'next/image';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import '@rainbow-me/rainbowkit/styles.css';
 
 export default function Home() {
   return (
     <div className='flex flex-col justify-center items-center h-screen'>
-      <Image
-        alt='Logo'
-        src="/logo.png"
-        width={200}
-        height={200}
-      />
-      <h1 className='text-4xl font-bold'>Welcome to</h1>
-      <h1 className='text-4xl font-bold'>Lenstalk</h1>
-      <button className='btn btn-wide'>Connect Wallet</button>
+      <Image alt='Logo' src='/logo.png' width={200} height={200} />
+      <h1 className='text-4xl font-bold mt-1'>Welcome to</h1>
+      <h1 className='text-4xl font-bold bg-gradient-to-r text-transparent bg-clip-text from-green-500 to-green-300 mb-5'>
+        Lenstalk
+      </h1>
+      <ConnectButton />
     </div>
-  )
+  );
 }
