939f20059e1612f3d6fa9628cf4639cfcfc1db5b
Lmmmmm <lm839377386@gmail.com>
2023-04-15 14:53:11 +0900

    fix: messages ui

--- src/app/chat/Messages.tsx
+++ src/app/chat/Messages.tsx
@@ -18,7 +18,8 @@ export default function Messages({ readerClient }: MessagesProps) {
       sent: new Date(),
     } as DecodedMessage,
   ]);
-
+  const [message, setMessage] = useState('');
+  const accountId = '0x0000000000000000000000000000000000000001';
   useEffect(() => {
     if (!readerClient) return;
 
@@ -35,32 +36,68 @@ export default function Messages({ readerClient }: MessagesProps) {
     streamMessages();
   }, [readerClient?.conversations]);
 
-  const onEnterPress = (e) => {
-    if (e.keyCode == 13 && e.shiftKey == false) {
-      e.preventDefault();
-      this.myFormRef.requestSubmit();
+  const handleSend = () => {
+    if (message.length > 0) {
+      setMessages((prevMessages) => {
+        const messages = [...prevMessages];
+        messages.push({
+          id: '12321',
+          senderAddress: '0x0000000000000000000000000000000000000001',
+          content: message,
+          sent: new Date(),
+        } as DecodedMessage);
+        return messages;
+      });
+      setMessage('');
     }
-  }
+  };
+
   return (
     <div className='divide-y h-full'>
       <div className='h-2/3 m-4'>
-        {messages.map((msg) => (
-          <div className='chat chat-start'>
-            <div className='chat-image avatar'>
-              <div className='w-10 rounded-full'>
-                <IdenticonImg username={msg.senderAddress} width={40} height={40} />
+        {messages.map((msg, index) =>
+          msg.senderAddress !== accountId ? (
+            <div key={index} className='chat chat-start'>
+              <div className='chat-image avatar'>
+                <div className='w-10'>
+                  <IdenticonImg username={msg.senderAddress} width={40} height={40} />
+                </div>
               </div>
+              <div className='chat-header inline-flex gap-2 p-1'>
+                <p>{truncateEthAddress(msg.senderAddress)}</p>
+                <time className='text-xs opacity-50'>{msg.sent.toLocaleTimeString()}</time>
+              </div>
+              <div className='chat-bubble'>{msg.content}</div>
             </div>
-            <div className='chat-header inline-flex gap-2 p-1'>
-              <p>{truncateEthAddress(msg.senderAddress)}</p>
-              <time className='text-xs opacity-50'>{msg.sent.toLocaleTimeString()}</time>
+          ) : (
+            <div key={index} className='chat chat-end'>
+              <div className='chat-image avatar'>
+                <div className='w-10 rounded-full'>
+                  <IdenticonImg username={msg.senderAddress} width={40} height={40} />
+                </div>
+              </div>
+              <div className='chat-header inline-flex gap-2 p-1'>
+                <p>{truncateEthAddress(msg.senderAddress)}</p>
+                <time className='text-xs opacity-50'>{msg.sent.toLocaleTimeString()}</time>
+              </div>
+              <div className='chat-bubble'>{msg.content}</div>
             </div>
-            <div className='chat-bubble'>{msg.content}</div>
-          </div>
-        ))}
+          ),
+        )}
       </div>
-      <div className='h-1/3'>
-        <textarea placeholder='Bio' className='textarea h-full w-full' onKeyDown={(e) => onEnterPress(e)}></textarea>
+      <div className='h-1/5 relative p-2'>
+        <div className='h-full w-full'>
+          <textarea
+            placeholder='Type message here'
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            className='textarea h-full w-full'
+            style={{ resize: 'none' }}
+          ></textarea>
+        </div>
+        <button onClick={() => handleSend()} className='absolute right-0 mt-2 btn'>
+          Send
+        </button>
       </div>
     </div>
   );
